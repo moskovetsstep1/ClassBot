40a3b62ba7186badc54595176142b8f1d2423fa6
Step [one] <moskovets1965@gmail.com>
2018-07-30 12:54:32 +0300

    Добавьте файлы проекта.

---
+++ Testbot/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Testbot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Bot = new MazeBot("557358914:AAE03Faw9-BwKFygJHFMl530FiGH9sPvB6Y");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
